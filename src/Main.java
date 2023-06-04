73320796f3093c698e01a4ca5e2c3d4c4b10ec75
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-06-04 12:56:24 +0300

    Main.java cheange

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 
         int price = 14000; // стоимость перелета
-        System.out.printf ((price) + " Стоимость перелета");
+        System.out.printf((price) + " Стоимость перелета");
         System.out.println();
         int bonus = 20; //Начисление одного бонуса с 20 рублей
-        System.out.printf((price/bonus) + " Миль начисленно за покупку");
+        System.out.print((price / bonus) + " Миль начисленно за покупку");
 
     }
 }
