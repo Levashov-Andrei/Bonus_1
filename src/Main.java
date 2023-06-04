3c3c453050b688d116e271a67589930b093ef1cb
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-06-04 12:46:06 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int price = 14000; // стоимость перелета
+        System.out.printf ((price) + " Стоимость перелета");
+        System.out.println();
+        int bonus = 20; //Начисление одного бонуса с 20 рублей
+        System.out.printf((price/bonus) + " Миль начисленно за покупку");
+
+    }
+}
